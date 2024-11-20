85f72dd03bdef6daee2168ab74a317af94e7644c
Alberto21-boop <albertobarbosa0003@gmail.com>
2024-11-20 12:26:03 -0300

    Input component finalized.

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -9,7 +9,8 @@ export const BaseInput = styled.input`
   background: transparent;
   border: 1px solid color ${(props) => props.theme.colors['base-header']};
   border-radius: 20px;
-  width: 80%;
+  width: 100%;
   height: 72px;
   padding-left: 7px;
+  font-size: 30px;
 `;
